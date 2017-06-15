d48bb089a04829eddd3d869591d0cbd5f17d3cad
Andreas Mieke <andreas@1750studios.com>
2017-06-15 23:30:32 +0200

    Adapting symbols file name and adding crashreports endpoint

--- endpoints.go
+++ endpoints.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path"
 	"strconv"
 	"strings"
@@ -56,9 +59,51 @@ func GetSymfile(c *gin.Context) {
 
 // PostCrashreports processes crashreport
 func PostCrashreports(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"title": "Crashreports",
+	file, _, err := c.Request.FormFile("upload_file_minidump")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  err.Error(),
+		})
+		return
+	}
+	defer file.Close()
+	var Crashreport database.Crashreport
+	database.Db.Create(&Crashreport)
+	filepath := path.Join(config.C.ContentDirectory, "Crashreports")
+	os.MkdirAll(filepath, 0755)
+	f, err := os.Create(path.Join(filepath, strconv.Itoa(int(Crashreport.ID))+".dmp"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+	io.Copy(f, file)
+	f.Close()
+	cmd := exec.Command("./minidump-stackwalk/stackwalker", path.Join(filepath, strconv.Itoa(int(Crashreport.ID))+".dmp"), path.Join(config.C.ContentDirectory, "Symfiles"))
+	//cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+	log.Print(out.String())
+	Crashreport.Product = c.Request.FormValue("prod")
+	Crashreport.Version = c.Request.FormValue("ver")
+	database.Db.Save(&Crashreport)
+	c.JSON(http.StatusCreated, gin.H{
+		"status": http.StatusCreated,
+		"error":  "OK",
+		"object": Crashreport,
 	})
+	return
 }
 
 // PostSymfiles processes symfile
@@ -73,23 +118,9 @@ func PostSymfiles(c *gin.Context) {
 	}
 	defer file.Close()
 	var Symfile database.Symfile
-	database.Db.Create(&Symfile)
-	filepath := path.Join(config.C.ContentDirectory, "Symfiles")
-	os.MkdirAll(filepath, 0755)
-	f, err := os.Create(path.Join(filepath, strconv.Itoa(int(Symfile.ID))+".sym"))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"status": http.StatusInternalServerError,
-			"error":  err.Error(),
-		})
-		return
-	}
-	defer f.Close()
-	io.Copy(f, file)
-	f.Seek(0, 0)
-	scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(file)
 	scanner.Scan()
-	if err := scanner.Err(); err != nil {
+	if err = scanner.Err(); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status": http.StatusInternalServerError,
 			"error":  err.Error(),
@@ -108,7 +139,20 @@ func PostSymfiles(c *gin.Context) {
 	Symfile.Arch = parts[2]
 	Symfile.Code = parts[3]
 	Symfile.Name = parts[4]
-	database.Db.Save(&Symfile)
+	filepath := path.Join(config.C.ContentDirectory, "Symfiles", Symfile.Name, Symfile.Code)
+	os.MkdirAll(filepath, 0755)
+	f, err := os.Create(path.Join(filepath, Symfile.Name+".sym"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+	defer f.Close()
+	file.Seek(0, 0)
+	io.Copy(f, file)
+	database.Db.Create(&Symfile)
 	c.JSON(http.StatusCreated, gin.H{
 		"status": http.StatusCreated,
 		"error":  "OK",
