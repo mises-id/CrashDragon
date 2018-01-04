e37d6a2c24dbf28bebaf7674f178cf070ffce4ea
Andreas Mieke <andreas@1750studios.com>
2018-01-04 23:18:48 +0100

    Add ALTER TABLE statements to migration

--- migrations/0.0.0-1.0.0.sql
+++ migrations/0.0.0-1.0.0.sql
@@ -1,8 +1,9 @@
 -- Fix database change introduced in commit c564789 to fix issue #6
+ALTER TABLE versions ADD git_repo text;
 UPDATE versions 
     SET git_repo = (
         SELECT git_repo
         FROM products
         WHERE versions.product_id = products.id
     );
-
+ALTER TABLE products DROP COLUMN git_repo;
